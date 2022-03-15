556258d4064b9ea3ec23c3dd5eb8f3d4bef933ab
Muhammad Ali <alim13@mcmaster.ca>
2022-03-14 20:33:02 -0400

    Initial Commit

---
+++ ramadanToDo/ramadanToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ramadanToDoApp.swift
+//  ramadanToDo
+//
+//  Created by Muhammad Ali on 2022-03-14.
+//
+
+import SwiftUI
+
+@main
+struct ramadanToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
